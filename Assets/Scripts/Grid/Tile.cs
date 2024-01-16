8fac23b9b8b86cf52104542ebc68f38d171c844c
robeyworks <s.robey@outlook.com>
2024-01-16 00:21:04 -0500

    oof ouch owie me eyes
    tiles are sprites now

--- Assets/Scripts/Grid/Tile.cs
+++ Assets/Scripts/Grid/Tile.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
+
+    [SerializeField] private Sprite _baseSprite, _offsetSprite;
     [SerializeField] private Color _baseColor, _offsetColor;
     [SerializeField] private SpriteRenderer _renderer;
     [SerializeField] private GameObject _highlight;
@@ -31,14 +33,17 @@ public class Tile : MonoBehaviour
         else
         {
             _highlight.SetActive(false);
-            _renderer.color = _isOffset ? _offsetColor : _baseColor;
+
+            //_renderer.color = _isOffset ? _offsetColor : _baseColor;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = _isOffset ? _offsetSprite : _baseSprite;
         }
 
     }
 
     public void Init(bool isOffset)
     {
-        _renderer.color = isOffset ? _offsetColor : _baseColor;
+        //_renderer.color = isOffset ? _offsetColor : _baseColor;
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = _isOffset ? _offsetSprite : _baseSprite;
         _isOffset = isOffset;
 
     }
@@ -49,12 +54,13 @@ public class Tile : MonoBehaviour
 
             if (Input.GetKey(KeyCode.LeftControl))
             {
-                _renderer.color = Color.clear;
+                //_renderer.color = Color.clear;
             }
             else
             {
-                _renderer.color = _isOffset ? _offsetColor : _baseColor;
-            }
+            //_renderer.color = _isOffset ? _offsetColor : _baseColor;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = _isOffset ? _offsetSprite : _baseSprite;
+        }
 
     }
 
